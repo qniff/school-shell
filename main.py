8c28471905b0dbb06ca1018317fd1f66059daa95
qniff <leonov.97a@gmail.com>
2019-09-24 08:37:50 +0200

    new functionality test

--- main.py
+++ main.py
@@ -5,34 +5,35 @@ import npyscreen
 import sys
 import os
 
-
+# APP
 class App(npyscreen.NPSAppManaged):
     def onStart(self):
         self.addForm("MAIN", MainForm, name="Welcome to StudentShell")
-        self.addForm("NOTE", NoteForm, name="Notes")
+        self.addForm("SEE-NOTES", NoteForm, name="Notes")
+        self.addForm("ADD-NOTES", NoteForm, name="Notes")
 
     def change_form(self,name):
         self.switchForm(name)
         self.resetHistory()
 
 
+# FORMS
 class MainForm(npyscreen.ActionForm):
     def create(self):
         self.seeNotesButton = self.add(SeeNotesButton, name="See notes")
+        self.seeNotesButton = self.add(AddNotesButton, name="Add notes")
         self.randomButton = self.add(npyscreen.Button, name="Random")
-        
+
     def on_ok(self):
         self.parentApp.switchForm(None)
-
 
 class NoteForm(npyscreen.ActionForm):
     def create(self):
         self.notePager = self.add(npyscreen.Pager, name="note", editable=False)
-        # note = open("nut", "r").readline()
 
         with open("nut") as f:
             note = f.readlines()
-        
+
         # note = os.popen("cat nut").read()
         self.notePager.values = [note]
     def on_ok(self):
@@ -45,8 +46,12 @@ class NoteForm(npyscreen.ActionForm):
 # MAIN BUTTONS
 class SeeNotesButton(npyscreen.ButtonPress):
     def whenPressed(self):
-        self.parent.parentApp.switchForm("NOTE")
-        
+        self.parent.parentApp.switchForm("SEE-NOTES")
+
+class AddNotesButton(npyscreen.ButtonPress):
+    def whenPressed(self):
+        self.parent.parentApp.switchForm("ADD-NOTES")
+# RUN
 if __name__ == "__main__":
     App = App()
     App.run()
